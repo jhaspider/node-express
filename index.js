18917a4ff5013f9771fbc694cc0404215cbf2b2c
Amarjit <jha.amarjit@gmail.com>
2024-10-16 20:26:48 +0530

    Port restored to 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all requests
 app.use(cors());
